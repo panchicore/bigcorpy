f0759766ae1793e38a91a05e62ee2039b9002749
Luis Pallares <thepanchi@gmail.com>
2020-12-07 10:33:26 -0500

    use env vars for the config file

--- settings.py
+++ settings.py
@@ -1,4 +1,9 @@
-DEFAULT_LIMIT = 100
-DEFAULT_OFFSET = 0
+import os
+
+DEFAULT_LIMIT = os.environ.get('DEFAULT_LIMIT', 100)
+DEFAULT_OFFSET = os.environ.get('DEFAULT_OFFSET', 0)
 DEFAULT_EXPAND = []
-EMPLOYEE_API_URL_BASE = "https://rfy56yfcwk.execute-api.us-west-1.amazonaws.com/bigcorp"
+EMPLOYEE_API_URL_BASE = os.environ.get(
+    'EMPLOYEE_API_URL_BASE',
+    "https://rfy56yfcwk.execute-api.us-west-1.amazonaws.com/bigcorp"
+)
